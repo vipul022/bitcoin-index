055fe3ad08e88dd3e03ed64121828cadbd10f514
vipul022 <vipul.seth022@gmail.com>
2020-11-09 12:52:45 +1100

    added styling with react-chartjs-2, chart.js and styled-components

--- src/App.js
+++ src/App.js
@@ -1,34 +1,73 @@
-import React, { useEffect, useState } from 'react'
+// import React, { useEffect, useState } from 'react'
+// import CurrencySelector from './components/CurrencySelector'
+// import DataDisplay from './components/DataDisplay'
+
+// function App() {
+//   const defaultCurrency = "AUD"
+//   const [currency, setCurrency] = useState(defaultCurrency)
+//   const [bitcoinData, setBitcoinData] = useState({})
+  
+//   function currencyChangeHandler(newCurrency){
+//     console.log("new currency: ", newCurrency)
+//     setCurrency(newCurrency)
+//   }
+
+//   const bitcoinApi = "https://api.coindesk.com/v1/bpi/historical/close.json"
+//   useEffect (()=>{
+//     console.log("running useEffect")
+//     fetch(`${bitcoinApi}?currency=${currency}`)
+//       .then(response => response.json())
+//       .then(data => setBitcoinData(data.bpi) )
+//       .catch(e => e)
+    
+//   }, [currency])
+
+//   return (
+//     <div>
+//       <h1>Bitcoin index</h1>
+//       <CurrencySelector currency={currency} handleCurrencyChange={currencyChangeHandler}/>
+//       <DataDisplay data={bitcoinData}/>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, {useState, useEffect} from 'react'
 import CurrencySelector from './components/CurrencySelector'
 import DataDisplay from './components/DataDisplay'
+import {AppContainer} from './components/StyledComponents'
+import Header from './components/Header'
 
-function App() {
-  const defaultCurrency = "AUD"
-  const [currency, setCurrency] = useState(defaultCurrency)
-  const [bitcoinData, setBitcoinData] = useState({})
-  
-  function currencyChangeHandler(newCurrency){
-    console.log("new currency: ", newCurrency)
-    setCurrency(newCurrency)
-  }
+const App = () => {
+	function currencyChangeHandler(currency) {
+		console.log("currency:", currency)
+		setCurrency(currency);
+	}
 
-  const bitcoinApi = "https://api.coindesk.com/v1/bpi/historical/close.json"
-  useEffect (()=>{
-    console.log("running useEffect")
-    fetch(`${bitcoinApi}?currency=${currency}`)
-      .then(response => response.json())
-      .then(data => setBitcoinData(data.bpi) )
-      .catch(e => e)
-    
-  }, [currency])
+	const defaultCurrency = "AUD"
+	const [currency,setCurrency] = useState(defaultCurrency)
+	const [bitcoinData, setBitcoinData] = useState({})
 
-  return (
-    <div>
-      <h1>Bitcoin index</h1>
-      <CurrencySelector currency={currency} handleCurrencyChange={currencyChangeHandler}/>
-      <DataDisplay data={bitcoinData}/>
-    </div>
-  );
+	const bitcoinApi = "https://api.coindesk.com/v1/bpi/historical/close.json"
+	useEffect(() => {
+		console.log("inside of useEffect")
+		function getData() {
+			fetch(`${bitcoinApi}?currency=${currency}`)
+			.then(response => response.json())
+			.then(data => setBitcoinData(data.bpi))
+			.catch(e => console.error(e))
+		}
+		getData()
+	},[currency])
+
+	return (
+		<AppContainer >
+			<Header title="BITCOIN PRICE INDEX"/>
+			<CurrencySelector currency={currency} handleCurrencyChange={currencyChangeHandler} />
+			<DataDisplay data={bitcoinData}/>
+		</AppContainer>
+	)
 }
 
-export default App;
+export default App
--- src/components/CurrencySelector.js
+++ src/components/CurrencySelector.js
@@ -1,17 +1,36 @@
+// import React from 'react'
+// import currencies from '../supported-currencies.json'
+
+// const CurrencySelector = ({currency, handleCurrencyChange}) =>{
+//     return (
+//         <div>
+//             <span>Select your currency</span>
+//             <select value={currency} onChange={(event)=> handleCurrencyChange(event.target.value)}>
+//                 {currencies.map((obj,index)=>
+//                     <option key={`${index}-${obj.country}`} value={obj.currency}>{obj.country}</option>
+//                 )}
+//             </select>
+//         </div>
+//     )
+// }
+
+// export default CurrencySelector
+
 import React from 'react'
 import currencies from '../supported-currencies.json'
+import {SelectContainer, SelectHeading} from './StyledComponents'
 
-const CurrencySelector = ({currency, handleCurrencyChange}) =>{
-    return (
-        <div>
-            <span>Select your currency</span>
-            <select value={currency} onChange={(event)=> handleCurrencyChange(event.target.value)}>
-                {currencies.map((obj,index)=>
-                    <option key={`${index}-${obj.country}`} value={obj.currency}>{obj.country}</option>
-                )}
-            </select>
-        </div>
-    )
+const CurrencySelector = ({currency, handleCurrencyChange}) => {
+
+  return (
+	  <SelectContainer>
+        <SelectHeading> Select your currency: </SelectHeading>
+        <select value={currency} onChange={(event) => {handleCurrencyChange(event.target.value)}}>
+          {currencies.map((obj, index) =>
+            <option key={`${index}-${obj.country}`} value={obj.currency}> {obj.country} </option>
+          )}
+        </select>
+      </SelectContainer>
+  )
 }
-
 export default CurrencySelector
